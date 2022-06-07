61d283c4d61e0e3412c19277d8750a0d068cd943
helan <helanmah@gmail.com>
2022-06-07 14:05:40 -0700

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, finish) {
+  let total = 0;
+  if (start < 0 || finish < 0) return "ERROR";
+  else if (typeof start == "string" || typeof finish == "string") {
+    return "ERROR";
+  }
+  else if (start < finish) {
+    for (let i = start; i <= finish; i++) {
+      total += i;
+    }
+  }
+  else if (start > finish) {
+    for (let i = finish; i <= start; i++) {
+      total += i;
+    }
+  }
+  else return "ERROR";
+  return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
