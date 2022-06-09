d5378dbf197acb828db4c92ca8d93d6b491207e9
helan <helanmah@gmail.com>
2022-06-09 15:52:00 -0700

    finished all the exercises #12 & #13 need some work but i have to move on

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let living = people.filter(person => (person.yearOfDeath == undefined))
+  //if (living.length > 0) {
+    let ages = people.map(person =>
+      (person.yearOfDeath - person.yearOfBirth));
+    let oldest = 0;
+    for (i in ages) {
+      if (ages[i] > oldest) {
+        oldest = i;
+      };
+    }
+    return people[oldest - 1];
+  //}
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,13 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const strSplit = str.split('');
+  const encodedLetters = [];
+  for (i in strSplit) {
+    const position = alphabet.indexOf(strSplit[i].toLowerCase())
+    encodedLetters.push(alphabet[position + shift])
+  }
+  const encodedStr = encodedLetters.join("")
+  return encodedStr;
 };
 
 // Do not edit below this line
